7aae84b1e9c67e7102f049667e5c93497cfb5c48
omar-abrash <omarabrash988@gmail.com>
2024-12-16 19:51:46 +0300

    install express and add small code

--- app.js
+++ app.js
@@ -1,79 +1,22 @@
-const http = require("http");
-const path = require("path");
-const fs = require("fs");
-
+const express = require("express");
 const PORT = 8080;
 
-const server = http.createServer((req, res) => {
-  const { url, method } = req;
+// deceleration server (application)
+const app = express();
 
-  if (url === "/" && method === "GET") {
-    res.write(`<div> Welcome In Home Page</div>`);
-  }
-  //
-  else if (url === "/add-user" && method === "GET") {
-    res.setHeader("Content-Type", "text/html");
-    res.write(
-      `
-      <form action="/users" method="POST">      
-      <input type="text" name="userName" />
-      <input type="submit" />
-      </form>
-        `
-    );
-  }
-  // payload // body
-  else if (url === "/users" && method === "POST") {
-    // req  ::> payload : body (userName=omar)
-    let payload = [];
+// url , method
+// general Midillware  : .use
+// middlewares         : .get , .post , .put , .patch , .delete
 
-    req.on("data", (chunk) => {
-      payload.push(chunk);
-    });
-
-    req.on("end", () => {
-      const data = Buffer.concat(payload).toString();
-      //  userName=o
-      const userName = data.split("=")[1]; // [userName , o]
-      // read file in this place :
-      const usersFile = path.join(__dirname, "./users.json");
-      const usersData = fs.readFileSync(usersFile, { encoding: "utf-8" });
-      const usersParsing = JSON.parse(usersData);
-      usersParsing.push(userName);
-      const usersText = JSON.stringify(usersParsing);
-      fs.writeFileSync(usersFile, usersText);
-    });
-
-    // res.writeHead(302, { Location: "/users" }); // 302 Found
-    // return res.end();
-    res.end();
-  }
-  //
-  else if (url === "/users" && method === "GET") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-
-    const usersFile = path.join(__dirname, "./users.json");
-    const usersData = fs.readFileSync(usersFile, { encoding: "utf-8" });
-    const users = JSON.parse(usersData);
-
-    res.write(`${users.map((user) => `<div>${user}</div>`)}`);
-  }
-  // exeption error
-  else {
-    res.write(`<div>Error Page</div>`);
-    return res.end();
-  }
-  return res.end();
+app.get("/", (req, res, next) => {
+  res.write("<div>Welcome in Home Page!</div>");
+  res.end();
 });
 
-server.listen(PORT, () => console.log("Server is running on PORT : " + PORT));
+// exeption error
+app.use((req, res, next) => {
+  console.log("Not Found Any Routes !");
+  res.end();
+});
 
-// CRUD Operators   create , read , update , delete
-
-// GET ALL : GET ITEMS from System
-// GET one some element (/id)
-
-// POST: add new user   (payload => userName) ++
-// PUT : modify user    (payload => userName , id )
-// BATCH : change user  (payload => userName, id )
-// DELETE : delete user (dynamic paramters : id )
+app.listen(PORT, () => console.log("App Server is running on " + PORT));
